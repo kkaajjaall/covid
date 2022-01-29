188fbaeae5ed918ee61ca0f774da177d533f7a79
KAJAL NIMJE <kajalnimje1998@gmail.com>
2022-01-29 15:40:39 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react'
 import Banner from './Banner';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <Form/>
     </div>
   );
 }
